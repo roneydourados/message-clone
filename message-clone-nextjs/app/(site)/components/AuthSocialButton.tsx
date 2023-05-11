047a33ef5ae401d23ea09422b4e6bfb5d01155b4
roneydourados <roneydourados@gmail.com>
2023-05-10 22:28:55 -0400

    ajustes

--- message-clone-nextjs/app/(site)/components/AuthSocialButton.tsx
+++ message-clone-nextjs/app/(site)/components/AuthSocialButton.tsx
@@ -9,9 +9,6 @@ interface AuthSocialButtonProps {
   onClick: () => void
 }
 
-const styleClass = 'inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm' +
-  'ring-1 ring-inset ring-gray-300 hover:ring-gray-50 focus:outline-offset-0'
-
 export const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
   icon: Icon,
   onClick
@@ -20,7 +17,22 @@ export const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
     <button
       type="button"
       onClick={onClick}
-      className={styleClass}
+      className="
+    inline-flex
+    w-full
+    justify-center
+    rounded-md
+    bg-white
+    px-4
+    py-2
+    text-gray-500
+    shadow-sm
+    ring-1
+    ring-inset
+    ring-gray-300
+    hover:ring-gray-50
+    focus:outline-offset-0
+    "
     >
       <Icon />
     </button>
